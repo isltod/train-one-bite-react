ab1bf563c645247e2ac0fc62c8036739b5a971f8
E. K. Lee <wolf@teoal.net>
2025-11-10 01:17:48 +0900

    다시 다시 시작

---
+++ chapter1/index.js
@@ -0,0 +1,26 @@
+console.log("Hello World!");
+
+let age = 25;
+console.log(age);
+
+// 놀랍게도 재선언은 잡아낸다..아래는 오류
+// let age;
+let age1;
+age1 = 25;
+console.log(age1);
+
+/*
+여러줄 주석은 이렇게
+*/
+
+const user_name = "이응경";
+const $ = "이런 것도 변수 이름이 되네...";
+console.log(user_name);
+console.log($);
+
+/*
+이런 건 의외로 안된다..
+let my-name = "이응경";
+console.log(my-name);
+*/
+
