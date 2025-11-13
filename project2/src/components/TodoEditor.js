4e64949ba7c83639d8b34794a3d8e405cd5d960a
E. K. Lee <wolf@teoal.net>
2025-11-13 23:46:44 +0900

    6장 프로젝트 실습 완료

---
+++ project2/src/components/TodoEditor.js
@@ -0,0 +1,41 @@
+import "./TodoEditor.css";
+import {useRef, useState} from "react";
+
+function TodoEditor({ onCreateTodo }) {
+    const [content, setContent] = useState("");
+    function onChangeContent(e) {
+        setContent(e.target.value);
+    }
+
+    const inputRef = useRef(null);
+    function onSubmit() {
+        if (!content) {
+            inputRef.current.focus();
+        } else {
+            onCreateTodo(content);
+            setContent("");
+        }
+    }
+    function onKeyDown(e) {
+        if (e.key === "Enter") {
+            onSubmit()
+        }
+    }
+
+    return (
+        <div className="TodoEditor">
+            <h4>새로운 Todo 작성하기 📜🖍</h4>
+            <div className="editor-wrapper">
+                <input ref={inputRef}
+                       value={content}
+                       onChange={onChangeContent}
+                       onKeyDown={onKeyDown}
+                       placeholder="새로운 Todo..."
+                />
+                <button onClick={onSubmit}>추가</button>
+            </div>
+        </div>
+    );
+}
+
+export default TodoEditor;
